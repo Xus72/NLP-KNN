9691288ee85f75935e0703078b413655856936b3
Xus72 <jexusth@hotmail.com>
2021-01-27 13:04:24 +0100

    Lee ficheros de una carpeta

--- main.hs
+++ main.hs
@@ -5,15 +5,18 @@ import System.Directory
 import Control.Exception
 import Data.Char (toLower)
 import System.IO.Error
-
+import Data.List
+--main0 :: IO ()
 main0 = do
     args <- getArgs
     if length args > 0 then do
         let carpeta = head args
         existeCarpeta <- doesDirectoryExist carpeta
         if existeCarpeta then do
-            ficheros <- getDirectoryContents carpeta
-            print ficheros
+            ficheros <- listDirectory carpeta
+            let prefijo = map ((carpeta ++ "/") ++) ficheros
+            contents <- traverse readFile prefijo
+            print contents
         else
             putStrLn ("El directorio " ++ carpeta ++ " no existe")
     else do
